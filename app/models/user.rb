32dbcc1e3b3f91ce6196c7b63356b40f3d1308c6
rodilon <renanodilon@gmail.com>
2018-04-15 11:04:16 -0300

    Controller de user com os métodos edit, show e update criados. Model de User foi modificado para acertar erro com validação de e-mail. Mass Assignment configurado no controller de user para aceitar a passagem dos parametros vindos da view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-    EMAIL_REGEXP = /\A[^@]+@([^@\.]\.)+[^@\.]+\z/
+    EMAIL_REGEXP = /\A\S+@.+\.\S+\z/
 
     validates_presence_of :email, :full_name, :location, :password
     validates_confirmation_of :password
-    validates_length_of :bio, minimum: 30, allow_blank: false
+    validates_length_of :bio, minimum: 10, allow_blank: false
     validate :email_format
     validates_uniqueness_of :email
     
